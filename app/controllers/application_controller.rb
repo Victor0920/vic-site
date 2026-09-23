f5d42fee715f110c7075424c99d1e8a2b619f8c3
Victor0920 <vvidal@snapfinance.co.uk>
2026-09-23 13:34:59 +0200

    Last tweaks to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
-
   # Pick the locale from ?locale=es, falling back to the default for
   # anything missing or unknown (I18n.with_locale raises on unknown locales).
   def switch_locale(&action)
